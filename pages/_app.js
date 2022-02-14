d53232080c1c7b36ce84d088c937d1ed25392544
Tapas Adhikary <tapas.adhikary@gmail.com>
2022-02-14 22:50:31 +0530

    ✨ Added guard for apis

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,14 @@
+
+import React from 'react'
+import SuperTokensReact from 'supertokens-auth-react'
+import { frontendConfig } from '../config/frontendConfig'
 import '../styles/globals.css'
+
+
+if (typeof window !== 'undefined') {
+  // we only want to call this init function on the frontend, so we check typeof window !== 'undefined'
+  SuperTokensReact.init(frontendConfig())
+}
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
