4dfcf319a6349835163e1543b18ea2079085ff3a
Tapas Adhikary <tapas.adhikary@gmail.com>
2022-02-15 08:55:01 +0530

    ✨ Now we have  a quotes API that auth guarded

---
+++ pages/api/quotes.js
@@ -0,0 +1,19 @@
+import supertokens from 'supertokens-node';
+import { superTokensNextWrapper } from 'supertokens-node/nextjs';
+import { verifySession } from 'supertokens-node/recipe/session/framework/express';
+import { backendConfig } from '../../config/backendConfig';
+import * as quoteList from '../../data/quotes.json';
+
+supertokens.init(backendConfig())
+
+export default async function quotes(req, res) {
+  await superTokensNextWrapper(
+    async (next) => {
+      return await verifySession()(req, res, next)
+    },
+    req,
+    res
+  )
+
+  return res.json(quoteList.quotes)
+}
