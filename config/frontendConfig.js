964099c5271dd277a77bc29f27b6e52ed540e529
Tapas Adhikary <tapas.adhikary@gmail.com>
2022-03-07 12:28:27 +0530

    🔥 init is not required in frontend config

--- config/frontendConfig.js
+++ config/frontendConfig.js
@@ -1,18 +1,15 @@
-import SuperTokensReact from "supertokens-auth-react";
-import PasswordlessReact from 'supertokens-auth-react/recipe/passwordless';
-import SessionReact from 'supertokens-auth-react/recipe/session';
-import { appInfo } from './appInfo';
+import PasswordlessReact from "supertokens-auth-react/recipe/passwordless";
+import SessionReact from "supertokens-auth-react/recipe/session";
+import { appInfo } from "./appInfo";
 
 export const frontendConfig = () => {
-if (typeof window !== 'undefined') {
-  SuperTokensReact.init({
-      appInfo,
-      recipeList: [
-          PasswordlessReact.init({
-              contactMethod: "EMAIL_OR_PHONE"
-          }),
-          SessionReact.init()
-      ]
-  });
-}
-}
+  return {
+    appInfo,
+    recipeList: [
+      PasswordlessReact.init({
+        contactMethod: "EMAIL_OR_PHONE",
+      }),
+      SessionReact.init(),
+    ],
+  };
+};
